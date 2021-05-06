1309ce609ea3fe6e6de447e8f713a633a90c6379
WaduGreg <l0cksit@gmail.com>
2021-05-06 16:19:43 +0200

    Initial commit

---
+++ packages-to-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+ 
+echo "Installing packages"
+ 
+pack_list=("i3" "xterm" "ubuntu-title-fonts" "konversation" "sysstat")
+ 
+for package in ${pack_list[*]}; do
+        echo "installing $package"
+        dnf install -y $package
+done
+ 
+echo "All packages are installed"
